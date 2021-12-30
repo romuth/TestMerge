85ffa478e4ee07e4b8cce522e7973e794138235f
Roman Kozhevnikow <roman.kozhevnikow@microfocus.com>
2021-12-30 15:57:53 +0200

    initial commit

---
+++ src/main/java/core/User.java
@@ -0,0 +1,20 @@
+package core;
+
+import java.util.Objects;
+
+public class User {
+    private String name;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {return true;}
+        if (o == null || getClass() != o.getClass()) {return false;}
+        User user = (User) o;
+        return Objects.equals(name, user.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+}
