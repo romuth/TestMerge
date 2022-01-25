32ca26f33e7b95c8234d8ec09537d968168fe066
Roman Kozhevnikow <roman.kozhevnikow@microfocus.com>
2022-01-25 12:13:20 +0200

    one

--- src/main/java/core/Main.java
+++ src/main/java/core/Main.java
@@ -1,4 +1,41 @@
 package core;
 
+import java.util.Arrays;
+import java.util.UUID;
+
+import org.json.JSONObject;
+
 public class Main {
+    public static void main(String[] args) {
+        Object[][] obj = new Object[][]{
+                {new JSONObject("{\"executionTimeout\":\"3\"}"), true},
+                {new JSONObject("{\"executionTimeoutMinutes\":\"3\"}"), false},
+                {new JSONObject(), false},
+                {new JSONObject("{\"executionTimeoutMinutes\":\"1\"}"), true},
+                {new JSONObject("{\"executionTimeoutHours\":\"1\"}"), true},
+                {new JSONObject("{\"executionTimeoutHours\":\"1\"}"), false},
+                {new JSONObject(), false},
+                {
+                        new JSONObject(
+                                "{\"executionTimeoutHours\":\"1\",\"executionTimeoutMinutes\":\"5\"}"),
+                        true
+                },
+                {
+                        new JSONObject(
+                                "{\"executionTimeoutHours\":\"1\",\"executionTimeoutMinutes\":\"5\"}"),
+                        false
+                },
+                {
+                        new JSONObject(
+                                "{\"executionTimeoutHours\":\"1\",\"executionTimeoutMinutes\":\"1\"}"),
+                        true
+                },
+                {new JSONObject("{\"executionTimeout\":\"61\"}"), false}
+        };
+        for (Object[] objects : obj) {
+            for (Object object : objects) {
+                System.out.println(object);
+            }
+        }
+    }
 }
